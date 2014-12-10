1799a85ca02f2800a4d4b28cca1867e751643812
Michael Economy <meconomy@goodreads.com>
2014-12-09 19:34:23 -0800

    Initial Commit

---
+++ dodgecat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  dodgecat
+//
+//  Created by Economy, Michael on 12/9/14.
+//  Copyright (c) 2014 Economy, Michael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ dodgecat/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  dodgecat
+//
+
+//  Copyright (c) 2014 Economy, Michael. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
